f32b87b92bdadf0088d7ac4537daa9d4f7fdf16f
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-04-09 23:08:46 +0200

    bug fix

--- demo.js
+++ demo.js
@@ -7,6 +7,11 @@ process.stdin.on("data",buffer=>{
 		.toString("utf-8")
 		.trim()
 	);
+	if(path==="q"){process.exit(0)}
 	const files=getDirFiles.getFiles(path);
+	if(!files){
+		process.stdout.write(`Nicht Gefunden!\n$ `);
+		return;
+	}
 	process.stdout.write(`${files.length} Dateien Gefunden!\n${files.map(item=>item+"\n").join("")}$ `);
 });
--- index.js
+++ index.js
@@ -6,34 +6,33 @@ function getFiles(dir){
 	let directories=[dir];
 	let foundDirectories;
 	const files=[];
-	do{
-		let scannedDirectories=[];
-		foundDirectories=[];
-		for(const directory of directories){
-			scannedDirectories.push(directory);
-			let itemsInDirectory;
-			try{
+	try{
+		do{
+			let scannedDirectories=[];
+			foundDirectories=[];
+			for(const directory of directories){
+				scannedDirectories.push(directory);
+				let itemsInDirectory;
 				itemsInDirectory=fs.readdirSync(directory);
-			}catch(e){
-				console.log(`cant read directory ${directory}\n\n`);
-				throw e;
+				for(let item of itemsInDirectory){
+					item=directory+item;
+					const stat=fs.lstatSync(item);
+					if(stat.isFile()) files.push(item);
+					else if(stat.isDirectory()) foundDirectories.push(item+sub);
+					else console.log("unknown item: "+item);
+				}
 			}
-			for(let item of itemsInDirectory){
-				item=directory+item;
-				const stat=fs.lstatSync(item);
-				if(stat.isFile()) files.push(item);
-				else if(stat.isDirectory()) foundDirectories.push(item+sub);
-				else console.log("unknown item: "+item);
-			}
+			directories=[
+				...directories.filter(directory=>
+					!scannedDirectories.includes(directory)
+				),
+				...foundDirectories,
+			];
 		}
-		directories=[
-			...directories.filter(directory=>
-				!scannedDirectories.includes(directory)
-			),
-			...foundDirectories,
-		];
+		while(foundDirectories.length>0);
+	}catch(e){
+		return null;
 	}
-	while(foundDirectories.length>0);
 	return files;
 }
 function filterFiles(files,types){

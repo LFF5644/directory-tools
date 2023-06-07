97791e2337c5bc3de10b6ff0cbef3448d483843d
LFF5644 <meerschweinchen5644@gamil.com>
2023-06-07 22:59:06 +0200

    add "getFolderItems" also called "ls" xD

--- index.js
+++ index.js
@@ -78,10 +78,41 @@ function makeDirectoriesInPath(path){
 		makeDirectory(createDir);
 	}
 }
+function getFolderItems(folder){
+	if(!folder.endsWith("/")) folder+="/";
+	let items;
+	try{
+		items=fs.readdirSync(folder);
+	}catch(e){
+		console.log("cant read folder/directory");
+		return null;
+	}
+	const newItems=[];
+	for(const item of items){
+		const itemPath=folder+item;
+		let stat;
+		try{
+			stat=fs.lstatSync(itemPath);
+		}catch(e){
+			console.log("cant get informations about "+item+" add to unknown");
+			newItems.push([item,"unknown"]);
+			continue;
+		}
+		if(stat.isBlockDevice()) newItems.push([item,"blockDevice"]);
+		else if(stat.isCharacterDevice()) newItems.push([item,"characterDevice"]);
+		else if(stat.isDirectory()) newItems.push([item,"directory"]);
+		else if(stat.isFile()) newItems.push([item,"file"]);
+		else if(stat.isSocket()) newItems.push([item,"socket"]);
+		else if(stat.isSymbolicLink()) newItems.push([item,"symbolicLink"]);
+		else newItems.push([item,"other"]);
+	}
+	return newItems;
+}
 
 module.exports={
 	filterFiles,
 	getFiles,
+	getFolderItems,
 	makeDirectoriesInPath,
 	makeDirectory,
 };
